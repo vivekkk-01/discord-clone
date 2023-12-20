23076e6133f6d7fadafe4353b379f3a62d741da3
vivek <chimnanivivek14@gmail.com>
2023-12-20 18:03:57 +0530

    specified env and cors in backend

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening at ${process.env.PORT}`);
