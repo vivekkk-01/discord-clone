bd3d2974476809814c0939cee58b931be1948a93
vivek <chimnanivivek14@gmail.com>
2023-12-25 15:24:51 +0530

    Added jwtToken while connecting to socket server

--- client/src/communication/socketServer.js
+++ client/src/communication/socketServer.js
@@ -3,7 +3,11 @@ import io from "socket.io-client";
 let socket = null;
 
 export const connectWithSocketServer = (userDetails) => {
-  socket = io("http://localhost:5000");
+  socket = io("http://localhost:5000", {
+    auth: {
+      token: userDetails.token,
+    },
+  });
 
   socket.on("connect", () => {
     console.log("connected with server...", socket);
--- client/src/pages/dashBoard/Dashboard.jsx
+++ client/src/pages/dashBoard/Dashboard.jsx
@@ -21,7 +21,7 @@ const Dashboard = () => {
     if (!userDetails) {
       navigate("/auth/login");
     } else {
-      connectWithSocketServer();
+      connectWithSocketServer(userDetails);
     }
   }, [userDetails]);
 
