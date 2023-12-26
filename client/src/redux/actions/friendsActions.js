94a1a82d4d3fe2b5036a1de65dec3ac04c7324c6
vivek <chimnanivivek14@gmail.com>
2023-12-26 17:49:46 +0530

    fixed minor issue in friendsActions.js file in frontend

--- client/src/redux/actions/friendsActions.js
+++ client/src/redux/actions/friendsActions.js
@@ -4,7 +4,7 @@ import { setError, setPendingFriendsInvitations } from "../slices/friendsSlice";
 
 export const sendFriendRequestAction =
   (requestData, closeDialogHandler) => async (dispatch) => {
-    const { type, response } = await sendFriendRequest(requestData);
+    const { type, response } = await sendFriendRequest({ email: requestData });
     if (type === "error") return dispatch(setError(response.data));
     closeDialogHandler();
     dispatch(setAlertOpen("Invitation has been sent!"));
