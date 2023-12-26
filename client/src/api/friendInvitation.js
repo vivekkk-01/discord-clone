77153bb5bc8a0efc45f348d740a50e814a8a7bdc
vivek <chimnanivivek14@gmail.com>
2023-12-26 17:43:26 +0530

    fixed some minor issues in frontend

--- client/src/api/friendInvitation.js
+++ client/src/api/friendInvitation.js
@@ -1,11 +1,22 @@
 import axios from "axios";
 const friendsClient = axios.create({
-  baseURL: "http://localhost:5000/api/friends",
+  baseURL: "http://localhost:5000/api/friend-invitation",
 });
 
 export const sendFriendRequest = async (requestData) => {
+  const userDetails = JSON.parse(localStorage.getItem("discord-user"));
   try {
-    const { data } = await friendsClient.post("/post-invitation");
+    const { data } = await friendsClient.post(
+      "/invite",
+      {
+        requestData,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${userDetails.token}`,
+        },
+      }
+    );
     return {
       type: "success",
       response: data,
--- client/src/redux/actions/friendsActions.js
+++ client/src/redux/actions/friendsActions.js
@@ -1,11 +1,11 @@
-import { sendFriendRequest } from "../../api/friends";
+import { sendFriendRequest } from "../../api/friendInvitation";
 import { setAlertOpen } from "../slices/alertSlice";
 import { setError, setPendingFriendsInvitations } from "../slices/friendsSlice";
 
 export const sendFriendRequestAction =
   (requestData, closeDialogHandler) => async (dispatch) => {
     const { type, response } = await sendFriendRequest(requestData);
-    if (type === "error") return setError(response);
+    if (type === "error") return dispatch(setError(response.data));
     closeDialogHandler();
     dispatch(setAlertOpen("Invitation has been sent!"));
   };
