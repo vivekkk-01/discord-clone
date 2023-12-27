54e6790b31caef38ef6d4b6ce064476a8dd7dfdd
vivek <chimnanivivek14@gmail.com>
2023-12-27 18:32:37 +0530

    fixed newConnectionHandler

--- server/socketHandlers/newConnectionHandler.js
+++ server/socketHandlers/newConnectionHandler.js
@@ -1,8 +1,14 @@
 const { addNewConnectedUser } = require("../serverStore");
+const {
+  updateFriendsAfterAccepting,
+  updateFriendsPendingInvitations,
+} = require("./updates/friends");
 
 const newConnectionHandler = (socket, io) => {
   const userDetails = socket.user;
   addNewConnectedUser({ socketId: socket.id, userId: userDetails.id });
+  updateFriendsPendingInvitations(userDetails.id);
+  updateFriendsAfterAccepting(userDetails.id);
 };
 
 module.exports = newConnectionHandler;
